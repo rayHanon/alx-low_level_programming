38ed6d80e94773c191756cafd7444b86616cb62e
rayHanon <rayhanon04@gmail.com>
2024-01-09 23:19:42 -0800

    printing sizes of various variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print the sizes of various types on the computer
+ * Return: 0
+ */
+int main(void)
+{
+	printf("size of char: %lu byte(s)\n", sizeof(char));
+	printf("size of int: %lu byte(s)\n", sizeof(int));
+	printf("size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
+
+	return(0);
+}
