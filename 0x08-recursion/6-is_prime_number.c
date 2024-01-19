ef2211892f7e42faef86eb0f8c610418108f8b8e
rayHanon <rayhanon04@gmail.com>
2024-01-19 03:22:59 -0800

    Finding prime numbers in C

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,11 +10,11 @@ int is_prime_number(int n)
 	int i;
 
 	if (n <= 1)
-		return(0);
+		return (0);
 	for (i = 2; i * i <= n; i++)
 	{
 		if (n % i == 0)
-			return(0);
+			return (0);
 	}
 	return (1);
 }
